e7d07696901fed4f9b06dd55321513b4a52563f5
Jongwoo Choi <muvnelik@naver.com>
2024-06-19 22:39:26 +0900

    [feature] Address 엔티티 추가  Mysql 의존성 추가

---
+++ src/main/java/com/jonggae/yakku/address/Address.java
@@ -0,0 +1,18 @@
+package com.jonggae.yakku.address;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "addresses")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Column(name = "address_detail", nullable = false)
+    private String addressDetail;
+}
