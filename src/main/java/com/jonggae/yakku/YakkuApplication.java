8c544cd816afa6b9b4ad7ef4311ed0910ba48b3f
Jongwoo Choi <muvnelik@naver.com>
2024-06-19 12:09:32 +0900

    [init] 프로젝트 생성 docker-compose, lombok, web, security 의존성 생성

---
+++ src/main/java/com/jonggae/yakku/YakkuApplication.java
@@ -0,0 +1,13 @@
+package com.jonggae.yakku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YakkuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YakkuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jonggae/yakku/YakkuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonggae.yakku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YakkuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
